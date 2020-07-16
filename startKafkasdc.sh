0a5f685d26831e6edc8bb01c358c036be959ccc8
Jibi Jose <jibijose@yahoo.com>
2020-07-17 03:44:25 +0530

    start stop scripts

--- startKafkasdc.sh
+++ startKafkasdc.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-sdc.yml -p sdc down
-echo "******************************************************************************************"
 docker-compose -f docker-compose-sdc.yml -p sdc up
--- startKafkawdc.sh
+++ startKafkawdc.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-wdc.yml -p wdc down
-echo "******************************************************************************************"
 docker-compose -f docker-compose-wdc.yml -p wdc up
